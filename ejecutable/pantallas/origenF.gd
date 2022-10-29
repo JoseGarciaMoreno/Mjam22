7c0257dce622c2e6d191dc1fe23d17710881cbf1
JoseGarciaMoreno <64783493+JoseGarciaMoreno@users.noreply.github.com>
2022-10-29 13:31:55 +0200

    bueno , seguridad

--- ejecutable/pantallas/origenF.gd
+++ ejecutable/pantallas/origenF.gd
@@ -1,13 +1,15 @@
 extends Node2D
-
-
 # Declare member variables here. Examples:
 # var a = 2
 var fuego = 0
 var banderaWin = false
 var banderaGM = false
 var inicio = true
-
+var limGOI = -5
+var limGOS = 16
+var limINF = 3
+var limSUP = 10
+var limH = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,25 +20,23 @@ func _ready():
 	$hud/mensa/TimerM.start(3)
 	$hud/caliente.hide()
 	$hud/fria.hide()
-	
+#	$hud/cargando.hide()
+#	print($hud/cargando.get_text(),)
+	$hud/cargando.text = "The beginning ..."
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+#func
 func _on_TextureButton_button_up():
 	fuego +=1
 	$AnimatedSprite.play("default")
 	$TimerCarga.start(0.35)
-	print("anima suma",fuego)
+#	print("anima suma",fuego)
 	
 	_comprobar()
-	if (fuego >3 and fuego <10 and !banderaWin):
-		print($hud/AnimatedSprite.show())
-		
+	if (fuego >limINF and fuego < limSUP and !banderaWin):
+#		print($hud/AnimatedSprite.show())
+		$hud/AnimatedSprite.show()
 		if($TimerControl.time_left == 0):
 			$TimerControl.start(4)
 			$hud/AnimatedSprite.show()
@@ -45,9 +45,7 @@ func _on_TextureButton_button_up():
 		$TimerControl.stop()
 		if(!banderaWin):
 			$hud/AnimatedSprite.hide()
-	
 	pass # Replace with function body.
-
 
 func _on_TimerCarga_timeout():
 	$AnimatedSprite.stop()
@@ -58,10 +56,10 @@ func _on_TimerCarga_timeout():
 
 func _on_TimerDescarga_timeout():
 	fuego -=1
-	if (fuego < -1 and ($hud/mensa/TimerInicio.time_left==0)):
+	if (fuego < limH and ($hud/mensa/TimerInicio.time_left==0)):
 		print("te hielas",fuego)
 		$hud/fria.show()
-		$hud/mensa.text = "te hielas"
+		$hud/mensa.text = "¡ You freeze with cold, hit it !"
 	else:
 		if (!$hud/mensa/TimerInicio.time_left==0):
 			print("te hielas",fuego)
@@ -69,8 +67,8 @@ func _on_TimerDescarga_timeout():
 			if(!banderaWin):
 				$hud/fria.hide()
 				$hud/mensa.text = ""
-	if (fuego < -4 ):
-		print("game over",fuego)
+	if (fuego < limGOI or fuego > limGOS):
+		print("game over ",fuego)
 		$hud/mensa.text = "GAME OVER"
 		if (!banderaGM):
 			banderaGM = true
@@ -79,14 +77,16 @@ func _on_TimerDescarga_timeout():
 	pass # Replace with function body.
 
 func _on_TimerControl_timeout():
-		if (fuego >3 and fuego <10 and !banderaWin):
+		if (fuego >limINF and fuego <limSUP and !banderaWin):
 			banderaWin = true
 			print("win",fuego)
-			$hud/mensa.text = "¡You WIN,  maquina!"
+			$hud/mensa.text = "¡You WIN! ,  ¡maquina!"
 			$hud/AnimatedSprite.show()
 			$hud/AnimatedSprite2.show()
 			$hud/AnimatedSprite2.play("fuegoA")
 			$hud/mensa/TimerM.start(3)
+			$hud/cargando.text  = "Loading  \n Please, wait"
+			$hud/cargando.show()
 			pass # Replace with function body.
 
 func _on_TimerM_timeout():
@@ -94,12 +94,14 @@ func _on_TimerM_timeout():
 	$hud/mensa/TimerM.stop()
 	if (banderaWin):
 		print ("saltar pantalla")
+#		print(get_tree().change_scene("res://pantallas/platafuego.tscn"))
 		print(get_tree().reload_current_scene())
 		pass # Replace with function body.
 
 func _on_TimerInicio_timeout():
 	$TimerDescarga.start(0.6)
 	$hud/mensa/TimerInicio.stop()
+	$hud/cargando.hide()
 	pass # Replace with function body.
 
 func _comprobar():
@@ -110,7 +112,7 @@ func _comprobar():
 		if (!banderaWin):
 			$hud/caliente.hide()
 			$hud/mensa.text = ""
-			if(fuego > 3 and fuego < 10):
+			if(fuego > limINF and fuego < limSUP):
 				$hud/mensa.text = "Good rhythm"
 			else:
 				$hud/mensa.text = ""
