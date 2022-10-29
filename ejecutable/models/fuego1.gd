e00e630ab1e41fefb3f3244703fd997a12b2a257
JoseGarciaMoreno <64783493+JoseGarciaMoreno@users.noreply.github.com>
2022-10-29 20:07:27 +0200

    segur

---
+++ ejecutable/models/fuego1.gd
@@ -0,0 +1,23 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+var quemando
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	scale_object_local(Vector3(1,1,5))
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area_body_entered(body):
+	print("detectando", body, str(body.name))
+	print("detectando")
+	pass # Replace with function body.
---
+++ ejecutable/player/lanza.gd
@@ -0,0 +1,17 @@
+extends MeshInstance
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+#	$fuego1.scale_object_local(Vector3(1,1,5))
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
